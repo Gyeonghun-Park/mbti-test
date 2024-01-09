d5a79adb9aeea61adb001586ea0a863548df3cc5
Gyeonghun Park <gp112795@gmail.com>
2024-01-09 18:31:35 +0900

    Update text color in page component

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { QnA } from "./_components/qna";
 
 export default function Home() {
   return (
-    <main className="flex w-full flex-col items-center p-6 [contain:paint] md:p-10">
+    <main className="flex w-full flex-col items-center text-pretty p-6 [contain:paint] md:p-10">
       <div className="w-full max-w-3xl">
         <QnA />
       </div>
